8e296c82ba022282d952dc9d9fbb6a593ac5b85a
Muhammad Rafi Ali <therafiali@gmail.com>
2025-06-20 01:01:15 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Hamza with Pipeline</h1>
+      <h1>Vite + THIS IS AUTO CHANGE</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
