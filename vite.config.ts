865af451e6b0ef53a7986b9d27bb99c1ccdb14d6
Muhammad Rafi Ali <therafiali@gmail.com>
2025-06-20 00:57:14 +0500

    dh

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 👈 This is CRITICAL
+    port: 3001
+  }
+
 })
