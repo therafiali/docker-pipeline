c038deeb20953c2ab92274728389db5b967088af
Muhammad Rafi Ali <therafiali@gmail.com>
2025-06-20 00:59:11 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and aoeuoeau to test HMR
+          Edit <code>src/App.tsx</code> and aoeuaoeueoauoaeuaoseuthaoes uhaoethuaoethuatoehu taoheu to test HMR
         </p>
       </div>
       <p className="read-the-docs">
